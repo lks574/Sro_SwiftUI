b8690c83637fc8bad839c3b9b94cf9f9e8a70e89
kyungseok.lee <kyungseok.lee@flitto.com>
2023-07-18 12:41:03 +0900

    feat: pokemon에 dependency 추가

--- Project/Study/Pokemon/Package.swift
+++ Project/Study/Pokemon/Package.swift
@@ -4,21 +4,37 @@
 import PackageDescription
 
 let package = Package(
-    name: "Pokemon",
-    platforms: [.iOS(.v15)],
-    products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "Pokemon",
-            targets: ["Pokemon"]),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "Pokemon"),
-        .testTarget(
-            name: "PokemonTests",
-            dependencies: ["Pokemon"]),
-    ]
+  name: "Pokemon",
+  platforms: [.iOS(.v15)],
+  products: [
+    .library(
+      name: "Pokemon",
+      targets: ["Pokemon"]),
+  ],
+  dependencies: [
+    .package(path: "../../Core/Architecture"),
+    .package(path: "../../Core/Domain"),
+    .package(path: "../../Core/Platform"),
+    .package(path: "../../Core/DesignSystem"),
+    .package(path: "../../Core/Functor"),
+    .package(
+      url: "https://github.com/pointfreeco/swift-composable-architecture",
+      .upToNextMajor(from: "0.56.0")),
+  ],
+  targets: [
+    .target(
+      name: "Pokemon",
+      dependencies: [
+        "Architecture",
+        "Domain",
+        "Platform",
+        "DesignSystem",
+        "Functor",
+        .product(name: "ComposableArchitecture", package: "swift-composable-architecture"),
+      ]
+    ),
+    .testTarget(
+      name: "PokemonTests",
+      dependencies: ["Pokemon"]),
+  ]
 )

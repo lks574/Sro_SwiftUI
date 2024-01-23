051f451ff8c35aca337f915c563c8ffd0b54871c
kyungseok.lee <lks574@naver.com>
2024-01-24 01:27:17 +0900

    feat: Theme Color 추가

---
+++ Project/Previews/TCABanking/Sources/Utils/Theme/Theme.swift
@@ -0,0 +1,28 @@
+import Foundation
+import SwiftUI
+
+struct Theme {
+  let colors = Colors()
+
+  struct Colors {
+    let background = Color("background")
+    let primary = Color("primary")
+    let primaryText = Color("primaryText")
+    let secondText = Color("secondText")
+  }
+}
+
+extension Theme {
+  static let `default` = Self()
+}
+
+private struct ThemeEnvironmentKey: EnvironmentKey {
+  static var defaultValue = Theme.default
+}
+
+extension EnvironmentValues {
+  var theme: Theme {
+    get { self[ThemeEnvironmentKey.self] }
+    set { self[ThemeEnvironmentKey.self] = newValue }
+  }
+}
